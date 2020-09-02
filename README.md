dc891e11e8f96b67bf3d445fde12ae64c580119e
JoshuaRobertson <josh.robertson@coinclear.uk>
2020-09-02 11:36:07 +0100

    Initial project commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# :musical_note: JS Drum Kit
+
+Designed in HTML & CSS, developed in JavaScript.
+
+## :pushpin: Summary
+
+Play the drums with your keyboard by listening for key events which then play the corresponding drum kit sound.
+
+### License
+
+Developed by: [Josh Robertson](https://github.com/JoshuaRobertson/)
---
+++ assets/js/drums.js
@@ -0,0 +1,27 @@
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key   = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+  // Stop function from running
+  if (!audio) return;
+
+  // Rewind audio to the start
+  audio.currentTime = 0;
+  audio.play();
+
+  // Highlight current key being played
+  key.classList.add('playing');
+};
+
+function removeTransition(e) {
+  // Skip if it's not a transform
+  if (e.propertyName !== 'transform') return;
+  this.classList.remove('playing');
+};
+
+// Loop over each key and attach an eventListener
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+// Play sound on key press
+window.addEventListener(['keydown'], playSound);
